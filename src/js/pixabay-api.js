2b5c163ab5b3fe70e241c19b54263d2d47b87233
Asprofil <adober.demchenko@gmail.com>
2024-06-06 19:58:10 +0300

    Final

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 const API_KEY = '44258731-7daca7e4db35e40c882241eb9';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
+export async function fetchImages(query, page = 1, perPage = 20) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
   const response = await fetch(url);
